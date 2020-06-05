8c94f4da2eadb09d27c4d44aa7101368e27bbd0e
krieglro <44705873+krieglro@users.noreply.github.com>
2020-06-05 14:12:26 +0200

    added population FeatureGroup

--- map.py
+++ map.py
@@ -24,7 +24,13 @@ map = folium.Map([1.3733,32.2903],width="97%",height="97%", zoom_start="7")
 v = folium.FeatureGroup(name="volcanoes")
 for na,la,lo,fe in zip(volcaName,volcanLat,volcanLon,feet):
     v.add_child(folium.CircleMarker(location=[la,lo],popup="{},{}ft".format(na,fe) ,radius=7,color='grey',fill_color=color_producer(fe), fill_opacity=0.7))
-p = folium.FeatureGroup(name ="population")
+
+p = folium.FeatureGroup(name="population")
+p.add_child(folium.GeoJson(data = open('world.json',mode='r',encoding="utf-8-sig").read(),
+style_function=lambda x:{'fillColor':'yellow' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+
+map.add_child(p)
 map.add_child(v)
 map.add_child(folium.LayerControl(position='topright',collapsed=True,))
 map.save("myMap.Html")
